e3a73a463aaeba65511113a2d44108a71cd02f24
Sammy Barasa <barsamms@gmail.com>
2017-07-10 18:27:11 +0300

    updates

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 //document root
 define('ROOT', dirname(__FILE__)); //set the root folder name based on current file location
 
-const DB_USER = 'spa'; //database username
-const DB_PASS = 'spa'; //user password
-const DB_SCHEMA = 'spa'; //schema name
+const DB_USER = 'xjcugjji_spa'; //database username
+const DB_PASS = 'Cyberhopper123'; //user password
+const DB_SCHEMA = 'xjcugjji_spa'; //schema name
 const DB_HOST = 'localhost'; //Database host localhost by default
 const DB_PORT = 3306; //3306 by default
 const DB_TYPE = 'mysql';
